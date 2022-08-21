774fd7c8e8b35c762d636c9757fa70dacd5adf7f
ngkw1224 <111679789+ngkw1224@users.noreply.github.com>
2022-08-21 17:49:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function initMap() {
   const hongkong = { lat: 22.3193, lng: 114.1694 };
   // The map, centered at hongkong
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
+    zoom: 22,
     center: hongkong,
   });
   // The marker, positioned at Hong Kong
