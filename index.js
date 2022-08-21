7ec3d69297bbff414017ac037149114bd39a4cb0
ngkw1224 <111679789+ngkw1224@users.noreply.github.com>
2022-08-21 17:31:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
+  // The location of HK
+  const HK = { lat: 22.3193, lng: 114.1694 };
+  // The map, centered at HK
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: uluru,
+    center: HK,
   });
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: HK,
     map: map,
   });
 }
